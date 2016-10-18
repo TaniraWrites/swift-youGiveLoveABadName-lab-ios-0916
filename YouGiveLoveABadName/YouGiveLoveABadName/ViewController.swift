fefdf40c49d2294cbdac847f5f89a82ea9686fb2
Taniraw <tanirawrites@gmail.com>
2016-10-18 13:53:51 -0400

    Finished

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+    
+    
+    @IBOutlet weak var bonJoviImage: UIImageView!
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer)  {
         
-
+        let totalSeconds = 1.0
+        
+        let durationOfAnimations = totalSeconds / 3.0
+        
+        let firstStartTime = 0.0
+        let secondStartTime = firstStartTime + durationOfAnimations
+        let thirdStartTime = secondStartTime + durationOfAnimations
+        
+        UIView.animateKeyframes(withDuration: totalSeconds, delay: 0.0,  animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: firstStartTime, relativeDuration: durationOfAnimations, animations: {
+                self.bonJoviImage.bounds.size.height = self.bonJoviImage.bounds.size.height * 0.5
+            })
+            UIView.addKeyframe(withRelativeStartTime: secondStartTime, relativeDuration: durationOfAnimations, animations: {
+                self.bonJoviImage.bounds.size.height = self.bonJoviImage.bounds.size.height  * 250
+            })
+            UIView.addKeyframe(withRelativeStartTime: thirdStartTime, relativeDuration: durationOfAnimations, animations: {
+                self.bonJoviImage.bounds.size.height = self.view.bounds.size.height
+            })
+            
+        })
+        
+        
+        
     }
-
+    
 }
 
